ff4501d6e4561db8dc7c0f9c41c2db8e86d4fc4f
Konduru Hemesh <159641878+Konduru-Hemesh@users.noreply.github.com>
2026-09-23 16:28:12 +0530

    Integrate nutrition info display for smoothie ingredients
    Added functionality to fetch and display nutrition information for selected fruits using an external API.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -13,7 +14,7 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-# Connect to Snowflake using SniS connection
+# Connect to Snowflake
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
@@ -25,12 +26,15 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
-# Process order
+# Process order and display nutrition info
 if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # Build SQL insert statement
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
